5994aa64aa0aad2ce1e0e847e5c95bbdb872cae8
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:29 +0530

    Initial commit: Weather-App-Mobile

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Weather-App-Mobile"</h1>
+        <p>"Native weather application"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
